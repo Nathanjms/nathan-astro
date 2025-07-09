4b24572706cd3deb5d7b30a1822b9cf78a343aba
Nathan James <nathan@nathanjms.co.uk>
2025-07-09 20:29:08 +0100

    add Vue dynamic island

---
+++ src/components/VueAutoTyperDemo.vue
@@ -0,0 +1,7 @@
+<template>
+    <AutoTyperVue componentTag="h2" class="text-xl" text="Auto Typer Vue3" :typingDelay="300" />
+</template>
+
+<style scoped>
+@import "auto-typer-vue3/dist/style.css";
+</style>
